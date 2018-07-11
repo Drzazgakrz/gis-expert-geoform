f03dceec85fbd0a4b0769d09f96ac688d0bf9526
Krzysztof <drzazga_krzysztof@o2.pl>
2018-07-11 10:04:25 +0200

    changed function to check errors

--- js/register.js
+++ js/register.js
@@ -41,7 +41,6 @@ var registerController = {
     showErrors: function (errors) {
         console.log(errors);
         Object.keys(errors).map(function (key, index) {
-            console.log(key);
             document.getElementById(key + 'Error').innerHTML = errors[key];
         });
     },
@@ -59,51 +58,28 @@ var registerController = {
         document.getElementById("zipCodeError").innerHTML = "";
     },
 
+    data : {
+        name : "Niepoprawne imię. Imię powinno zaczynać się z dużej litery, pozostałe małe",
+        lastName : "Niepoprawne nazwisko. Nazwisko powinno zaczynać się z dużej litery, pozostałe małe",
+        username : "Niepoprawny mail. mail powinien kończyć się @nazwa_domeny np @gmail.com",
+        password : "Niepoprawne hasło. Powinien mieć co najmniej 6 znaków",
+        confirmation : "Hasła się nie zgadzają",
+        phone : "Niepoprawny numer. Numer powinien mieć 9-11 cyfr",
+        street : "Niepoprawna nazwa ulicy. Nazwa powinna się zaczynać z duzej litery a reszta mała",
+        buildingNumber : "Niepoprawny numer budynku. Numer powinien być nieujemny",
+        flatNumber : "Niepoprawy numer mieszkania",
+        zipCode : "Niepoprawny kod pocztowy. Numer powinien być nieujemny",
+        city : "Niepoprawna nazwa miasta. Nazwa powinna się zaczynać z duzej litery a reszta mała"
+    },
     validateData: function () {
         var errors = {};
-        var name = document.getElementById("name");
-        if (!name.value.match(name.pattern)) {
-            errors.name = "Niepoprawne imię. Imię powinno zaczynać się z dużej litery, pozostałe małe";
-        }
-        var lastName = document.getElementById("lastName");
-        if (!lastName.value.match(lastName.pattern)) {
-            errors.lastName = "Niepoprawne nazwisko. Nazwisko powinno zaczynać się z dużej litery, pozostałe małe";
-        }
-        var email = document.getElementById("email");
-        if (!email.value.match(email.pattern)) {
-            errors.username = "Niepoprawny mail. mail powinien kończyć się @nazwa_domeny np @gmail.com";
-        }
-        var password = document.getElementById("password");
-        var confirmpassword = document.getElementById("confirmpassword");
-        if (password.value.length < 6) {
-            errors.password = "Niepoprawne hasło. Powinien mieć co najmniej 6 znaków";
-        }
-        if (password.value !== confirmpassword.value) {
-            errors.confirmation = "Hasła się nie zgadzają";
-        }
-        var phone = document.getElementById("phone").value;
-        if (!phone.match("(^[0-9]{9,11}$)")) {
-            errors.phone = "Niepoprawny numer. Numer powinien mieć 9-11 cyfr";
-        }
-        var street = document.getElementById("street").value;
-        if (!street.match("(^[A-Z ÀÁÂÃÄÅ ĄŻŹ ÒÓÔÕÖØ Ł Ć ĘŚ Ń ÈÉÊË Ç ÌÍÎÏ ÙÚÛÜ Ñ]{1})([a-zàáâãäåąźżòóÓłćęśńôõöøèéêëçìíîïùúûüÿñ]{2,29}$)")) {
-            errors.street = "Niepoprawna nazwa ulicy. Nazwa powinna się zaczynać z duzej litery a reszta mała";
-        }
-        var buildingNumber = document.getElementById("buildNr").value;
-        if (!buildingNumber.match("(^[0-9]{1,5}$)")) {
-            errors.buildingNumber = "Niepoprawny numer budynku. Numer powinien być nieujemny";
-        }
-        var flatNumber = document.getElementById("aptNr").value;
-        if (!flatNumber.match("(^[0-9]{0,5}$)") && flatNumber !== "") {
-            errors.flatNumber = "Niepoprawy numer budynku";
-        }
-        var zipCode = document.getElementById("zipCode").value;
-        if (!zipCode.match("(^[0-9]{2}-?[0-9]{3}$)")) {
-            errors.zipCode = "Niepoprawny kod pocztowy. Numer powinien być nieujemny";
-        }
-        var city = document.getElementById("city").value;
-        if (!city.match("(^[A-Z ÀÁÂÃÄÅ ĄŻŹ ÒÓÔÕÖØ Ł Ć ĘŚ Ń ÈÉÊË Ç ÌÍÎÏ ÙÚÛÜ Ñ ]{1})([a-zàáâãäåąźżòóÓłćęśńôõöøèéêëçìíîïùúûüÿñ]{2,29}$)")) {
-            errors.city = "Niepoprawna nazwa miasta. Nazwa powinna się zaczynać z duzej litery a reszta mała";
+        var keys = Object.keys(this.data);
+        for (var i = 0;i<keys.length;i++){
+            var input = document.getElementById(keys[i]);
+            if(!input.value.match(input.pattern)){
+                console.log(keys[i]);
+                errors[keys[i]] = this.data[keys[i]];
+            }
         }
         return errors;
     },
